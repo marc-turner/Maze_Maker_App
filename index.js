93d7f2ecd7e9e2cb04d4a6f1bc25072e508ade1a
Marc Turner <marc.t1967@gmail.com>
2020-02-12 11:59:40 -0500

    add starting grid code

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ World.add(world, walls);
 
 // Maze Generation
 
+const shuffle = arr => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+};
+
 const grid = Array(cells)
     .fill(null)
     .map(() => Array(cells).fill(false));
@@ -43,4 +58,32 @@ const horizontals = Array(cells - 1)
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
-const stepThroughCell = (row, column) => {};
+const stepThroughCell = (row, column) => {
+    if (grid[row][column]) {
+        return;
+    }
+    grid[row][column] = true;
+    const neighbors = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
+        if (
+            nextRow < 0 ||
+            nextRow >= cells ||
+            nextColumn < 0 ||
+            nextColumn >= cells
+        ) {
+            continue;
+        }
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
+    }
+};
+
+stepThroughCell(startRow, startColumn);
+// console.log(grid);
